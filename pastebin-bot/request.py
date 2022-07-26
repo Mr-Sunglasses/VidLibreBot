d3eff452e0694d1ce8cd7943ca1c1cd24266fb1e
Kanishk Pachauri <krishna03012003@icloud.com>
2022-07-26 18:42:26 +0530

    Contains code for file creation and deletion

---
+++ pastebin-bot/request.py
@@ -0,0 +1,12 @@
+import os
+import sys
+
+
+def file_writer(text):
+
+    with open('file.txt', 'w') as text_file:
+        text_file.write(text)
+
+def file_remover():
+    if os.path.exists("file.txt"):
+        os.remove("file.txt")  # one file at a time

4bd607f8dc1c966d457ff5392bbdd703a94b6385
Kanishk Pachauri <krishna03012003@icloud.com>
2022-07-27 01:54:03 +0530

    Logic of QR

---
+++ pastebin-bot/qr_generator.py
@@ -0,0 +1,14 @@
+# Import QRCode from pyqrcode
+import pyqrcode
+import png
+from pyqrcode import QRCode
+import os
+
+def generate_qr(link):
+    url = pyqrcode.create(link)
+
+    url.png('myqr.png', scale=6)
+
+def delete_qr():
+    if os.path.exists("myqr.png"):
+        os.remove("myqr.png")

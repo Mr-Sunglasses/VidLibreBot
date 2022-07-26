5f553ff45b0e0949f04b716de09b0531ba15bdb4
Kanishk Pachauri <krishna03012003@icloud.com>
2022-07-27 02:51:25 +0530

    Added love quotes happy and sad

---
+++ pastebin-bot/love_quotes.py
@@ -0,0 +1,60 @@
+happy_love_quotes = [
+    """ "For small creatures such as we, the vastness is bearable only through love." – Carl Sagan """,
+    """ "When you're a kid, you assume your parents are soulmates. My kids are gonna be right about that." – The Office """,
+    """ "So excuse me forgetting
+But these things, I do
+You see, I've forgotten
+If they're green or they're blue.
+Anyway, the thing is, what I really mean
+Yours are the sweetest eyes I've ever seen."
+– Elton John, "Your Song" """,
+    """ "Nobody has ever measured, not even poets, how much the heart can hold."
+– Zelda Fitzgerald """,
+    """ "Two people in love, alone, isolated from the world, that's beautiful." – Milan Kundera
+ """,
+    """ "True love stories never have endings." – Richard Bach """,
+    """ "The best and most beautiful things in the world cannot be seen or even touched—they must be felt with the heart." – Helen Keller """,
+    """ "Deep within us — no matter who we are — there lives a feeling of wanting to be lovable, of wanting to be the kind of person that others like to be with. And the greatest thing we can do is to let people know that they are loved and capable of loving." – Fred Rogers """,
+    """ "The greatest thing you'll ever learn is just to love and be loved in return." – Nat King Cole, "Nature Boy" lyrics """,
+    """ "Laughter is holier than piety, freedom is sweeter than fame, and in the end it's love and love alone that really matters." – Tom Robbins """,
+    """ "You know you're in love when you can't fall asleep because reality is finally better than your dreams." – Dr. Seuss """,
+    """ She had blue skin,
+And so did he.
+He kept it hid
+And so did she.
+They searched for blue
+Their whole life through,
+Then passed right by-
+And never knew."
+– Shel Silverstein, "Masks" """,
+    """ "You're my one in five billion." – The X-Files """,
+    """ "Happiness is anyone and anything at all that's loved by you." – You're a Good Man, Charlie Brown """,
+    """ "A friend is someone who knows all about you and still loves you." – Elbert Hubbard""",
+    """  "Maybe you don't need the whole world to love you, you know. Maybe you just need one person." – The Muppets """,
+    """ "That's why they call them crushes. If they were easy, they'd call them something else." – Sixteen Candles """,
+    """ "Once upon a time there was a boy who loved a girl, and her laughter was a question he wanted to spend his whole life answering." – Nicole Krauss, The History of Love """,
+    """ "Love is an irresistible desire to be irresistibly desired." – Robert Frost """,
+    """ "At the touch of love everyone becomes a poet." – Plato """,
+    """ "Love is the condition in which the happiness of another person is essential to your own." – Robert A. Heinlein, Stranger in a Strange Land """,
+    """ "But the heart's not like a box that gets filled up. It expands in size the more you love." – Her  """,
+    """ "There is no remedy for love, but to love more." – Thoreau """,
+]
+
+sad_love_quotes = [
+    """ 
+    Duniya mai ab ishq baaki nahi hai,
+    Bus ek shakhs inke liye kaafi nahi hai.
+    """,
+    """ 
+    Tum khud hi khud ko satate ho,
+    Dil kyu itni jaldi kisi se lagate ho.
+    """,
+    """ 
+    Sath hokar Phir bhi fikar karte hai,
+    Ab har kahi hum unka zikar karte hai.
+    """,
+    """ 
+    Kitno ne tujhse sawal kia hai,
+    Kya Kisi ne tera khayal bhi kiya hai?
+    """,
+]

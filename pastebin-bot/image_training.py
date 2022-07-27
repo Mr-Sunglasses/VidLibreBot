03d620406820066dc596ea132e6d5b5f31942651
Kanishk Pachauri <krishna03012003@icloud.com>
2022-07-27 16:35:13 +0000

    added model code

--- pastebin-bot/image_training.py
+++ pastebin-bot/image_training.py
@@ -1,1 +1,19 @@
 import tensorflow as tf
+from numpy import np
+import cv2
+
+
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
+x_train, x_test = x_train / 255, x_test / 255
+
+class_name = ['Plane', 'Car', 'Bird', 'Cat', 'Deer', 'Dog', 'Frog', 'Horse', 'Ship', 'Man', 'Women', 'Boy', 'Human']
+
+model = tf.keras.models.Sequential()
+model.add(tf.keras.layers.Conv2D(32, (3,3), activation='relu', input_shape = (32, 32, 3)))
+model.add(tf.keras.layers.MaxPooling2d((2,2)))
+model.add(tf.keras.layers.Conv2D(64, (3,3), activation='relu'))
+model.add(tf.keras.layers.MaxPooling2d((2,2)))
+model.add(tf.keras.layers.Conv2D(64, (3,3), activation='relu'))
+model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dense(64, activation='relu'))
+model.add(tf.keras.layers.Dense(10, activation='softmax'))

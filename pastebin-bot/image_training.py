b4eefd1dfca448d18daf24eb466605271296dea5
Kanishk Pachauri <krishna03012003@icloud.com>
2022-07-27 22:32:06 +0530

    fixes

--- pastebin-bot/image_training.py
+++ pastebin-bot/image_training.py
@@ -9,9 +9,9 @@ class_name = ['Plane', 'Car', 'Bird', 'Cat', 'Deer', 'Dog', 'Frog', 'Horse', 'Sh
 
 model = tf.keras.models.Sequential()
 model.add(tf.keras.layers.Conv2D(32, (3,3), activation='relu', input_shape = (32, 32, 3)))
-model.add(tf.keras.layers.MaxPool2d((2,2)))
+model.add(tf.keras.layers.MaxPool2D((2,2)))
 model.add(tf.keras.layers.Conv2D(64, (3,3), activation='relu'))
-model.add(tf.keras.layers.MaxPool2d((2,2)))
+model.add(tf.keras.layers.MaxPool2D((2,2)))
 model.add(tf.keras.layers.Conv2D(64, (3,3), activation='relu'))
 model.add(tf.keras.layers.Flatten())
 model.add(tf.keras.layers.Dense(64, activation='relu'))

23de858cfdf888e0ff5bf77c555a6cc95ed1630d
Kanishk Pachauri <krishna03012003@icloud.com>
2022-07-28 20:27:43 +0530

    Fixed Insta profiles

--- pastebin-bot/insta_profile.py
+++ pastebin-bot/insta_profile.py
@@ -1,17 +1,27 @@
 import instaloader
 import os
 import shutil
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 def profile_downloader(username):
+    name = os.getenv('username')
+    passw = os.getenv('password')
     ig = instaloader.Instaloader()
+    ig.login(name, passw)
     ig.download_profile(username, profile_pic_only=True)
 
 
 # dp = input("Enter Insta username : ")
+def get_all_images(username):
+    name = os.getenv('username')
+    passw = os.getenv('password')
+    ig = instaloader.Instaloader()
+    ig.login(name, passw)
+
+
 
 def profile_delete(username):
     shutil.rmtree(f'{username}')
-
-
-

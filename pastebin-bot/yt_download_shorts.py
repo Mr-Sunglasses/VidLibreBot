9f7bcdea580c28f5dd9a1038cf8a550044a339d0
Kanishk Pachauri <krishna03012003@icloud.com>
2022-07-28 16:26:53 +0530

    Yt download Feature

---
+++ pastebin-bot/yt_download_shorts.py
@@ -0,0 +1,11 @@
+import pafy
+import youtube_dl
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
+def download_video(link_of_video):
+    link = link_of_video
+    video = pafy.new(link)
+    best = video.getbest()
+    best.download()
+

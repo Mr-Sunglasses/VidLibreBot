ced7109f74bdfa973ca09a8ee299a2321e190ba6
Kanishk Pachauri <krishna03012003@icloud.com>
2022-08-01 01:11:21 +0530

    Format

--- pastebin-bot/insta_profile.py
+++ pastebin-bot/insta_profile.py
@@ -7,8 +7,8 @@ load_dotenv()
 
 
 def profile_downloader(username):
-    name = os.getenv('username')
-    passw = os.getenv('password')
+    name = os.getenv("username")
+    passw = os.getenv("password")
     ig = instaloader.Instaloader()
     ig.login(name, passw)
     ig.download_profile(username, profile_pic_only=True)
@@ -16,12 +16,11 @@ def profile_downloader(username):
 
 # dp = input("Enter Insta username : ")
 def get_all_images(username):
-    name = os.getenv('username')
-    passw = os.getenv('password')
+    name = os.getenv("username")
+    passw = os.getenv("password")
     ig = instaloader.Instaloader()
     ig.login(name, passw)
 
 
-
 def profile_delete(username):
-    shutil.rmtree(f'{username}')
+    shutil.rmtree(f"{username}")

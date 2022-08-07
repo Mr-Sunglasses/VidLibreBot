700a2cff78bf16c2848e8fdd85a10962c59decbe
Kanishk Pachauri <krishna03012003@icloud.com>
2022-08-07 11:59:23 +0530

    added fix

--- pastebin-bot/yt_download_shorts.py
+++ pastebin-bot/yt_download_shorts.py
@@ -1,20 +1,3 @@
-# import pafy
-# import youtube_dl
-# import ssl
-# ssl._create_default_https_context = ssl._create_unverified_context
-#
-# def download_video(link_of_video):
-#     link = link_of_video
-#     video = pafy.new(link)
-#     best = video.getbest()
-#     best.download()
-#
-# def get_title(link_of_video):
-#     link = link_of_video
-#     video = pafy.new(link)
-#     best = video.getbest()
-#     return best.title
-
 from pytube import YouTube
 import ssl
 

f63c373ae69050e252056af90d2b3acf28fa558c
Ayush Dubey <33064931+devAyushDubey@users.noreply.github.com>
2022-09-19 11:24:11 +0530

    Added login before fetching posts. Fixes Issue #16

--- pastebin-bot/insta_all_pictures.py
+++ pastebin-bot/insta_all_pictures.py
@@ -1,11 +1,23 @@
 from instaloader import Instaloader,Profile
 import threading
 import shutil
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 profile_instance = Instaloader()
 
+def login(profile):
+    name = os.getenv("username")
+    passw = os.getenv("password")
+    profile.login(name,passw)
+    
 
 def fetch_posts(user):
+
+    login(profile_instance)
 
     try:
         prof = Profile.from_username(profile_instance.context, user)
